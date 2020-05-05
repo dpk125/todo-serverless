655abfa69ff3d84bb84189a102b9ec34916dd83d
Daniil Pyšniak <dpk125@users.noreply.github.com>
2020-05-05 19:37:33 +0300

    Remove unnecessary comments

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -20,7 +20,7 @@ export const handler = async (
   logger.info('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
-    logger.info('User was authorized', jwtToken)
+    logger.info('User authorized', jwtToken)
 
     return {
       principalId: jwtToken.sub,
