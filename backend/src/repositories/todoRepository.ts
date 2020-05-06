1c54999f619484bb4c36f5fe20cdf7b5d7733e7f
Daniil Pyšniak <dpk125@users.noreply.github.com>
2020-05-06 17:03:28 +0300

    Fix todo repository add, update, remove and query

--- backend/src/repositories/todoRepository.ts
+++ backend/src/repositories/todoRepository.ts
@@ -6,57 +6,56 @@ import { TodoUpdate } from '../models/TodoUpdate';
 const xAWS = xRay.captureAWS(AWS);
 
 export class TodoRepository {
-    constructor(
-        private readonly documentClient = new xAWS.DynamoDB.DocumentClient(),
-        private readonly todosTable: string = process.env.TODOS_TABLE
-    ) {
-    }
+  constructor(
+    private readonly documentClient = new xAWS.DynamoDB.DocumentClient(),
+    private readonly todosTable: string = process.env.TODOS_TABLE,
+    private readonly todosIndex: string = process.env.TODOS_INDEX
+  ) {
+  }
 
-    async getByUserId(userId: string): Promise<TodoItem[]> {
-        const result = await this.documentClient.query({
-            TableName: this.todosTable,
-            KeyConditionExpression: 'userId = :userId',
-            ExpressionAttributeValues: {
-                ':userId': userId,
-            },
-            ScanIndexForward: false,
-        }).promise();
+  async getByUserId(userId: string): Promise<TodoItem[]> {
+    const result = await this.documentClient.query({
+      TableName: this.todosTable,
+      IndexName: this.todosIndex,
+      KeyConditionExpression: 'userId = :userId',
+      ExpressionAttributeValues: {
+        ':userId': userId,
+      },
+      ScanIndexForward: true,
+    }).promise();
 
-        return result.Items as TodoItem[];
-    }
+    return result.Items as TodoItem[];
+  }
 
-    async save(item: TodoItem) {
-        await this.documentClient.put({
-            TableName: this.todosTable,
-            Item: item
-        }).promise();
+  async save(item: TodoItem) {
+    await this.documentClient.put({
+      TableName: this.todosTable,
+      Item: item,
+    }).promise();
 
-        return item;
-    }
+    return item;
+  }
 
-    async remove(todoId: string, userId: string) {
-        await this.documentClient.delete({
-            TableName: this.todosTable,
-            ConditionExpression: 'userId = :userId',
-            ExpressionAttributeValues: {
-                ':userId': userId
-            },
-            Key: { todoId }
-        }).promise();
-    }
+  async remove(todoId: string, userId: string) {
+    await this.documentClient.delete({
+      TableName: this.todosTable,
+      Key: { todoId, userId },
+    }).promise();
+  }
 
-    async update(todoId: string, userId: string, item: TodoUpdate) {
-        await this.documentClient.update({
-            TableName: this.todosTable,
-            UpdateExpression: 'SET name = :name, dueDate = :dueDate, done = :done',
-            ConditionExpression: 'userId = :userId',
-            ExpressionAttributeValues: {
-                ':name': item.name,
-                ':dueDate': item.dueDate,
-                ':done': item.done,
-                ':userId': userId
-            },
-            Key: { todoId }
-        }).promise();
-    }
+  async update(todoId: string, userId: string, item: TodoUpdate) {
+    await this.documentClient.update({
+      TableName: this.todosTable,
+      UpdateExpression: 'SET #name = :name, dueDate = :dueDate, done = :done',
+      ExpressionAttributeValues: {
+        ':name': item.name,
+        ':dueDate': item.dueDate,
+        ':done': item.done,
+      },
+      ExpressionAttributeNames: {
+        '#name': 'name',
+      },
+      Key: { todoId, userId },
+    }).promise();
+  }
 }
